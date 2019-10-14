88be782704b6071fe775c5af7fd0ce6d602a857f
Otis <pandayusei@gmail.com>
2019-10-14 12:30:11 +0800

    初始化mall-admin模块

---
+++ mall-admin/src/main/java/com/j3n0/mall/MallAdminApp.java
@@ -0,0 +1,11 @@
+package com.j3n0.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallAdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApp.class, args);
+    }
+}
