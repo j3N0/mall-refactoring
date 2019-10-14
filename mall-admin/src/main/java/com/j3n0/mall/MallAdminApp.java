4abe9c73a587ff672fc67a36ccc397a4dbbc9ff9
Otis <pandayusei@gmail.com>
2019-10-14 18:52:12 +0800

    设置多个模块的应用能够访问同一个h2 database

--- mall-admin/src/main/java/com/j3n0/mall/MallAdminApp.java
+++ mall-admin/src/main/java/com/j3n0/mall/MallAdminApp.java
@@ -1,11 +1,20 @@
 package com.j3n0.mall;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class MallAdminApp {
     public static void main(String[] args) {
         SpringApplication.run(MallAdminApp.class, args);
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
     }
 }
