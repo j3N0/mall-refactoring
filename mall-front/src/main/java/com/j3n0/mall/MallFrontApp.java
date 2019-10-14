9be6b3254ae1029ee2e4705635cbaaae8d4510c6
Otis <pandayusei@gmail.com>
2019-10-14 13:59:00 +0800

    初始化mall-front模块, 设置mall-admin端口为8080，mall-front端口为8081

---
+++ mall-front/src/main/java/com/j3n0/mall/MallFrontApp.java
@@ -0,0 +1,11 @@
+package com.j3n0.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallFrontApp {
+    public static void main(String[] args) {
+        SpringApplication.run(MallFrontApp.class, args);
+    }
+}
