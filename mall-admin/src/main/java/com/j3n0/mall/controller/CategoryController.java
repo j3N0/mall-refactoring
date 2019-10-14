9585c23854c68646d49dc58755e446a1cb0314cf
Otis <pandayusei@gmail.com>
2019-10-14 13:54:28 +0800

    实现Category Product的Controller

--- mall-admin/src/main/java/com/j3n0/mall/controller/CategoryController.java
+++ mall-admin/src/main/java/com/j3n0/mall/controller/CategoryController.java
@@ -1,6 +1,11 @@
 package com.j3n0.mall.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.j3n0.mall.model.Category;
+import com.j3n0.mall.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CategoryController {
@@ -14,4 +19,50 @@ public class CategoryController {
     // update
 
     // delete
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/categories")
+    public List<Category> list() {
+        return categoryService.list();
+    }
+
+    @GetMapping("/categories/{id}")
+    public Category get(@PathVariable Long id) {
+        return categoryService.get(id);
+    }
+
+    @PostMapping("/categories")
+    public String create(@RequestBody Category category) {
+        int count = categoryService.create(category);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
+
+    @PutMapping("/categories/{id}")
+    public String update(@PathVariable Long id, @RequestBody Category category) {
+        int count = categoryService.update(id, category);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public String delete(@PathVariable long id) {
+        int count = categoryService.delete(id);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
 }
--- mall-admin/src/main/java/com/j3n0/mall/controller/ProductController.java
+++ mall-admin/src/main/java/com/j3n0/mall/controller/ProductController.java
@@ -1,6 +1,13 @@
 package com.j3n0.mall.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.j3n0.mall.model.Category;
+import com.j3n0.mall.model.Product;
+import com.j3n0.mall.service.CategoryService;
+import com.j3n0.mall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -14,4 +21,53 @@ public class ProductController {
     // update
 
     // delete
+
+    @Autowired
+    ProductService productService;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/categories/{cid}/products")
+    public List<Product> listByCategory(@PathVariable Long cid) {
+        return productService.listByCid(cid);
+    }
+
+    @GetMapping("/products/{id}")
+    public Product get(@PathVariable Long id) {
+        return productService.get(id);
+    }
+
+    @PostMapping("/products")
+    public String create(@RequestBody Product product) {
+        int count = productService.create(product);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
+
+    @PutMapping("/products/{id}")
+    public String update(@PathVariable Long id, @RequestBody Product product) {
+        int count = productService.update(id, product);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
+
+    @DeleteMapping("/products/{id}")
+    public String delete(@PathVariable long id) {
+        int count = productService.delete(id);
+
+        if (count > 0) {
+            return "success";
+        } else {
+            return "failed";
+        }
+    }
 }
